9d7c03c9e9911d773fc033330ec6350d20c49430
archimedon <ronniedz@icloud.com>
2018-05-11 20:30:22 -0500

    Not waiting for resolve

--- src/app.js
+++ src/app.js
@@ -28,12 +28,66 @@ app.use('/fmgr', require('./routes/fmgr'))
 
 
 const uploadFilter = new UploadFilter({ target: 'https://news.google.com/gn/news/?ned=us&gl=US&hl=en'});
+const hpm = require('http-proxy-middleware')
+const multiparty = require('multiparty');
 
 app.get('/test', (req, res) => {
     uploadFilter.goog().then(rem => rem.pipe(res));
 });
 
-app.use(uploadFilter.getProxy());
+
+// app.use(uploadFilter.getProxy());
+function read(req, ask) {
+    let form = new multiparty.Form({
+        encoding: "utf8",
+        maxFilesSize: 1024 ^ 3,   // num bytes. default is Infinity.
+        autoFields: true,        // Enables field events and disables part events for fields. This is automatically set to true if you add a field listener.
+        autoFiles: true          // Enables file events and disables part events for files. This is automatically set to true if you add a file listener.
+    });
+    
+    let uplargs = {
+        bucketId: '',
+        destDir: '',
+        author: encodeURIComponent('editor@org.com')
+    };
+    let purl = 'non'
+
+    function former(req) {
+        return new Promise(resolve => {
+
+             let tmp = 'non'
+             setTimeout(() => resolve(
+                form.parse(req, (err, fields, files) => {
+                    Object.keys(fields).forEach(function (name) {
+                    uplargs[name] = encodeURIComponent(fields[name][0]);
+                    console.log('got field named: ' + name);
+                    console.log('got field value: ' + fields[name][0]);
+                    });
+                    tmp = `/upload/${uplargs.bucketId}/${uplargs.author}/${uplargs.destDir}`;
+            })                 
+             ), 2000); 
+                
+                             
+
+                console.log('Upload completed!');
+                console.log(`purl: ${tmp}`);
+                //          res.setHeader('text/plain');
+                //          res.end('Received ' + files.length + ' files');
+        });
+    }
+    
+    return former(req)
+}
+
+app.post('/fmgr', (req, res) => {
+    let cal = {
+        val : 'empty',
+        store: (val) => { cal.val = val}
+    };
+    read(req, cal).then(dt => {cal.store(dt + 'ggg')})
+
+    res.end(cal.val)
+});
 
 const server = app.listen(process.env.PORT || config.port, () => {
     console.log(`Listening on: http://${server.address().address}:${server.address().port}`);
