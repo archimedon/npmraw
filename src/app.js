c49e9f09b78f4ba7895bd68ad879b62d23f52460
archimedon <ronniedz@icloud.com>
2018-05-12 20:20:46 -0500

    Simple Promised response

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 "use strict";
+require("babel-polyfill");
+
 // import http from 'http';
 import express from 'express';
 import config from './config.json';
 import logger from 'morgan';
 import UploadFilter from './middleware/upload_filter.js';
+
+const asyncHandler = require('express-async-handler')
+const request = require('request')
 
 var createError = require('http-errors');
 var path = require('path');
@@ -27,67 +32,125 @@ app.use(express.static(__dirname + '/public'))  // static directory
 app.use('/fmgr', require('./routes/fmgr'))
 
 
-const uploadFilter = new UploadFilter({ target: 'https://news.google.com/gn/news/?ned=us&gl=US&hl=en'});
-const hpm = require('http-proxy-middleware')
-const multiparty = require('multiparty');
+// const uploadFilter = new UploadFilter({ target: 'https://news.google.com/gn/news/?ned=us&gl=US&hl=en' });
+// const hpm = require('http-proxy-middleware')
+// const multiparty = require('multiparty');
 
-app.get('/test', (req, res) => {
-    uploadFilter.goog().then(rem => rem.pipe(res));
-});
-
-
-// app.use(uploadFilter.getProxy());
-function read(req, ask) {
-
-    function former(req) {
-        return new Promise(resolve => {
-            let form = new multiparty.Form({
-                encoding: "utf8",
-                maxFilesSize: 1024 ^ 3,   // num bytes. default is Infinity.
-                autoFields: true,        // Enables field events and disables part events for fields. This is automatically set to true if you add a field listener.
-                autoFiles: true          // Enables file events and disables part events for files. This is automatically set to true if you add a file listener.
-            });
-            
-            let uplargs = {
-                bucketId: '',
-                destDir: '',
-                author: encodeURIComponent('editor@org.com')
-            };
-            let purl = 'non'
-        
-             let tmp = 'non'
-             setTimeout(() => resolve(
-                form.parse(req, (err, fields, files) => {
-                    Object.keys(fields).forEach(function (name) {
-                    uplargs[name] = encodeURIComponent(fields[name][0]);
-                    console.log('got field named: ' + name);
-                    console.log('got field value: ' + fields[name][0]);
-                    });
-                    tmp = `/upload/${uplargs.bucketId}/${uplargs.author}/${uplargs.destDir}`;
-            })                 
-             ), 2000); 
-                
-                             
-
-                console.log('Upload completed!');
-                console.log(`purl: ${tmp}`);
-                //          res.setHeader('text/plain');
-                //          res.end('Received ' + files.length + ' files');
-        });
-    }
+function goog() {
     
-    return former(req)
+    return new Promise(resolve => {
+        request.get({uri: 'https://news.google.com/gn/news/?ned=us&gl=US&hl=en'}, function (error, gres, body) {
+            resolve(body);
+
+            
+        });
+    });
 }
 
-app.post('/fmgr', (req, res) => {
-    let cal = {
-        val : 'empty',
-        store: (val) => { cal.val = val}
-    };
-    read(req, cal).then(dt => {cal.store(dt + 'ggg')})
 
-    res.end(cal.val)
-});
+
+ 
+// express.get('/', asyncHandler(async (req, res, next) => {
+
+app.get('/test', asyncHandler(async (req, res, next) => {
+    // uploadFilter.goog().then(rem => rem.pipe(res));
+    // let str = await uploadFilter.goog();
+    let str = await goog();
+    console.log("str : " + str)
+
+    res.end("str : " + str)
+//     res.end(uploadFilter.goog())
+//     // const fetchResult = request('https://www.reddit.com/r/javascript/top/.json?limit=5')
+//     // uploadFilter.goog().then(rem => rem.pipe(res));
+//     res.end(uploadFilter.goog())
+
+}));
+
+
+// // app.use(uploadFilter.getProxy());
+// function read(req, ask) {
+
+//     function former(req) {
+//         return new Promise(resolve => {
+//             let form = new multiparty.Form({
+//                 encoding: "utf8",
+//                 maxFilesSize: 1024 ^ 3,   // num bytes. default is Infinity.
+//                 autoFields: true,        // Enables field events and disables part events for fields. This is automatically set to true if you add a field listener.
+//                 autoFiles: true          // Enables file events and disables part events for files. This is automatically set to true if you add a file listener.
+//             });
+
+//             let uplargs = {
+//                 bucketId: '',
+//                 destDir: '',
+//                 author: encodeURIComponent('editor@org.com')
+//             };
+//             let purl = 'non'
+
+//             let tmp = 'non'
+//             setTimeout(() => resolve(
+//                 form.parse(req, (err, fields, files) => {
+//                     Object.keys(fields).forEach(function (name) {
+//                         uplargs[name] = encodeURIComponent(fields[name][0]);
+//                         console.log('got field named: ' + name);
+//                         console.log('got field value: ' + fields[name][0]);
+//                     });
+//                     tmp = `/upload/${uplargs.bucketId}/${uplargs.author}/${uplargs.destDir}`;
+//                 })
+//             ), 2000);
+
+
+
+//             console.log('Upload completed!');
+//             console.log(`purl: ${tmp}`);
+//             //          res.setHeader('text/plain');
+//             //          res.end('Received ' + files.length + ' files');
+//         });
+//     }
+
+//     return former(req)
+// }
+
+// function resin(req, res) {
+//     let form = new multiparty.Form({
+//         encoding: "utf8",
+//         maxFilesSize: 1024 ^ 3,   // num bytes. default is Infinity.
+//         autoFields: true,        // Enables field events and disables part events for fields. This is automatically set to true if you add a field listener.
+//         autoFiles: true          // Enables file events and disables part events for files. This is automatically set to true if you add a file listener.
+//     });
+
+//     let uplargs = {
+//         bucketId: '',
+//         destDir: '',
+//         author: encodeURIComponent('editor@org.com')
+//     };
+
+//     let tmp = 'non'
+//     form.parse(req, (err, fields, files) => {
+//         Object.keys(fields).forEach(function (name) {
+//             uplargs[name] = encodeURIComponent(fields[name][0]);
+//             console.log('got field named: ' + name);
+//             console.log('got field value: ' + fields[name][0]);
+//         });
+//         tmp = `/upload/${uplargs.bucketId}/${uplargs.author}/${uplargs.destDir}`;
+//         res.end('Received ' + tmp);
+//     })
+// }
+
+// const asyncHandler = require('express-async-handler')
+// const request = require('request')
+
+// app.get('/test', (req, res) => {
+//     // const fetchResult = request('https://www.reddit.com/r/javascript/top/.json?limit=5')
+//     // uploadFilter.goog().then(rem => rem.pipe(res));
+//     res.end(uploadFilter.goog())
+// })
+
+
+ 
+// express.get('/', asyncHandler(async (req, res, next) => {
+//     const bar = await resin(req, res);
+//     res.send(bar)
+// }))
 
 const server = app.listen(process.env.PORT || config.port, () => {
     console.log(`Listening on: http://${server.address().address}:${server.address().port}`);
@@ -95,3 +158,16 @@ const server = app.listen(process.env.PORT || config.port, () => {
 
 export default app;
 
+
+// let start = new Promise((resolve, reject) => resolve("start"))
+// let middle = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(" middle"), 1000)
+// })
+// let end = new Promise((resolve, reject) => resolve(" end"))
+
+// Promise.all([start, middle, end]).then(results => {
+//   $("#output").append(results[0])
+//   $("#output").append(results[1])
+//   $("#output").append(results[2])
+// })
+// }
---
+++ src/middleware/upload_filter.js
@@ -0,0 +1,200 @@
+
+"use strict";
+// var express = require('express');
+
+// var router = express.Router();
+
+// router.get('/', function(req, res) {
+//     res.send('GET handler for /dogs route.');
+// });
+
+// router.post('/', function(req, res) {
+//     res.send('POST handler for /dogs route.');
+// });
+
+// module.exports = router;
+const request = require('request');
+
+const isUploadRegEx = new RegExp('(?:POST|PUT).*', 'i');
+const isProxyRegEx = new RegExp('^/cfs/*(.*)', 'i');
+
+
+
+const isUploadMethod = {
+    test: (pathname, req) => {
+        console.log("ctype: " + req.headers['content-type']);
+        return isUploadRegEx.test(req.method) && req.headers['content-type'].startsWith('multipart/form-data');
+    }
+}
+
+const isProxyPath = {
+    test: (pathname, req) => {
+        return isProxyRegEx.test(pathname);
+    }
+}
+
+class UploadFilter {
+ 
+    constructor(options) {
+
+        this.options = options || {};
+        this.options.aurl = '/freak'
+        let inpectForm = (req) => {
+        
+
+            let remres = function (rq) {
+                let multiparty = require('multiparty');
+            let form = new multiparty.Form({
+                encoding: "utf8",
+                maxFilesSize: 1024 ^ 3,   // num bytes. default is Infinity.
+                autoFields: true,        // Enables field events and disables part events for fields. This is automatically set to true if you add a field listener.
+                autoFiles: true          // Enables file events and disables part events for files. This is automatically set to true if you add a file listener.
+            });
+            let purl = 'non'
+                let uplargs = {
+                    bucketId: '',
+                    destDir: '',
+                    author: encodeURIComponent('editor@org.com')
+                };
+                return new Promise(resolve => {
+                
+                    form.parse(rq, (err, fields, files) => {
+                        Object.keys(fields).forEach(function (name) {
+                            uplargs[name] = encodeURIComponent(fields[name][0]);
+                            console.log('got field named: ' + name);
+                            console.log('got field value: ' + fields[name][0]);
+                        });
+                        purl = `/upload/${uplargs.bucketId}/${uplargs.author}/${uplargs.destDir}`;
+            
+                        console.log('Upload completed!');
+                        console.log(`purl: ${purl}`);
+                        resolve(purl);
+                        //          res.setHeader('text/plain');
+                        //          res.end('Received ' + files.length + ' files');
+                    });
+                });
+            }
+
+            remres(req).then(aurl => this.options.aurl = aurl)
+            return this.options.aurl;
+        }
+    
+
+        this.options.target = options.target || 'https://news.google.com/gn/news/?ned=us&gl=US&hl=en'
+
+        if (!options.fsroute) this.options.fsroute = '/cfs';    
+        if (!options.target) this.options.target = 'http://localhost:8080/cloudfs/api/v1';
+        if (!options.changeOrigin) this.options.changeOrigin = false;
+        if (!options.ws) this.options.ws = false;
+    
+        if (!options.pathRewrite) this.options.pathRewrite = function(path, req) {
+            let purl = '/junk'
+            if (isProxyRegEx.test(path)) {
+                purl = path.replace(isProxyRegEx, '/$1')
+                console.log('isProxyRegEx pathRewrite= ' + purl);
+            } else {
+                console.log('making redir');
+    
+    
+                purl = inpectForm(req);
+                console.log('purl', purl);
+            }
+            return purl;
+        }
+
+        
+        if (!options.onProxyReq) this.options.onProxyReq = (proxyReq, req, res, options) => {
+
+            purl = inpectForm(req);
+            console.log('purl', purl);
+
+            // proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+        };
+
+        if (!options.onProxyRes) this.options.onProxyRes = (proxyRes, req, res) => {
+            let body = "";
+    
+            proxyRes.on('data', function (data) {
+                data = data.toString('utf-8');
+                body += data;
+            })
+    
+            proxyRes.on('end', function (data) {
+                try {
+                    if (proxyRes.headers['content-type'].startsWith('application/json')) {
+                        data = JSON.parse(body);
+                        console.log("data:", data[0].downloadUrl);
+                    } else {
+                        data = body;
+                    }
+                } catch (err) { }
+            })
+        };
+    
+    }
+
+    goog() {
+        console.log('this.options', this.options);
+        
+        return new Promise(resolve => {
+            request.get({uri: this.options.target}, function (error, gres, body) {
+                resolve(body);
+            });
+         });
+    }
+
+   
+    
+    getProxy() {
+        var filter = (pathname, req) => {
+            var pathtest = isProxyPath.test(pathname, req);
+            var methodtest = isUploadMethod.test(pathname, req);
+            console.log("isProxyPath, '" + pathname + "' " + pathtest);
+            console.log("isUploadMethod, '" + req.method + "' " + methodtest);
+            return methodtest || pathtest;
+        }
+        const hpm = require('http-proxy-middleware')
+        
+        /**
+         * Create the proxy middleware, so it can be used in a server.
+         */
+        return hpm(filter, this.options);
+    }
+    
+}
+
+module.exports = UploadFilter;
+
+
+// parseForm(form, req) {
+
+
+//     return new Promise(resolve => {
+//         form.parse(req, (err, fields, files) => {
+//             Object.keys(fields).forEach(function (name) {
+//                 uplargs[name] = encodeURIComponent(fields[name][0]);
+//                 console.log('got field named: ' + name);
+//                 console.log('got field value: ' + fields[name][0]);
+//             });
+// //            Object.keys(files).forEach(function(fileFieldKey) {
+// //                var file = files[fileFieldKey][0];
+// //                console.log('got file fileFieldKey ' + fileFieldKey);
+// //                console.log('got file fileFieldName ' + file.fieldName);
+// //                console.log('got file originalFilename ' + file.originalFilename);
+// //                console.log('got file path ' + file.path);
+// //                console.log('got file file.headers ' + JSON.stringify(file.headers));
+// //                console.log('got file file.size ' + file.size);
+// //            });
+            
+//             purl = `/upload/${uplargs.bucketId}/${uplargs.author}/${uplargs.destDir}`;
+
+//             console.log('Upload completed!');
+//             console.log(`purl: ${purl}`);
+//             resolve(purl);
+//             //          res.setHeader('text/plain');
+//             //          res.end('Received ' + files.length + ' files');
+//         });
+//         return purl;
+//     });
+
+// }
